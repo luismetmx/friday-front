ae01dbc65d4c180af709a3de8e3058129490819c
Mario Castellanos <Mario.Castellanos@jda.com>
2019-10-19 14:30:53 -0500

    Refactored Mat stepper pt1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatStepperModule, MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule, MatSelect } from '@angular/material/select';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppEditTeamComponent } from './app-edit-team/app-edit-team.component';
 import { AppTaskListComponent } from './app-task-list/app-task-list.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppTeamListComponent } from './app-team-list/app-team-list.component';
 import { AppSolutionListComponent } from './app-solution-list/app-solution-list.component';
 import { AppPendingTasksListComponent } from './app-pending-tasks-list/app-pending-tasks-list.component';
@@ -15,6 +19,7 @@ import { TaskCountComponent } from './task-count/task-count.component';
 import { TaskListEditorComponent } from './task-list-editor/task-list-editor.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { NewProjectComponent } from './new-project/new-project.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -34,7 +39,13 @@ import { NewProjectComponent } from './new-project/new-project.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-project/new-project.component.ts
+++ src/app/new-project/new-project.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FrapiService } from '../frapi.service';
-
-
+import { FormControl, FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-new-project',
@@ -9,18 +8,26 @@ import { FrapiService } from '../frapi.service';
   styleUrls: ['./new-project.component.css']
 })
 export class NewProjectComponent implements OnInit {
+  formGroup;
 
-  projectName;
-  projectSolution;
-  projectType;
-  projectPhase;
+  projectData: FormGroup;
+  tasksData: FormArray;
+  resourcesData: FormArray;
+
 
   solutions;
   types = ['Go Nuts Migration', 'New Implementation', 'Upgrade', 'Migration', 'POC', 'Roll Out'];
   phases = ['ND', 'Prepare', 'Design', 'Construct', 'Validate', 'Deploy', 'Transition', 'Onboarding',
    'UAT', 'Cut Over', 'Hypercare', 'SIT'];
 
-  constructor(private frapiservice: FrapiService) { }
+  constructor(private frapiservice: FrapiService, private formBuilder: FormBuilder) {
+    this.projectData = this.formBuilder.group({
+      name : '',
+      solution : null,
+      type : null,
+      phase : null
+    });
+  }
 
   ngOnInit() {
     this.frapiservice.getSolutions().subscribe((data) => {
@@ -30,10 +37,6 @@ export class NewProjectComponent implements OnInit {
 
   public onSubmit(formData) {
     console.log(formData);
-  }
-
-  setSolution(solutionID) {
-    this.projectSolution = solutionID;
   }
 
 }
